fc10520663a7e3da41056f40b2278cb5deb05a84
Daniel Christ <dchrist0921@gmail.com>
2024-06-25 11:42:36 -0700

    brave update WIP

--- pkgs/applications/networking/browsers/brave/default.nix
+++ pkgs/applications/networking/browsers/brave/default.nix
@@ -6,16 +6,16 @@ callPackage ./make-brave.nix (removeAttrs args [ "callPackage" ])
     if stdenv.isAarch64 then
       {
         pname = "brave";
-        version = "1.67.116";
-        url = "https://github.com/brave/brave-browser/releases/download/v1.67.116/brave-browser_1.67.116_arm64.deb";
+        version = "1.67.119";
+        url = "https://github.com/brave/brave-browser/releases/download/v1.67.119/brave-browser_1.67.119_arm64.deb";
         hash = "sha256-YOvfXI+bSX3WZRBYXxGYhNme9NZl3ob/JYw6KshXn54=";
         platform = "aarch64-linux";
       }
     else if stdenv.isx86_64 then
       {
         pname = "brave";
-        version = "1.67.116";
-        url = "https://github.com/brave/brave-browser/releases/download/v1.67.116/brave-browser_1.67.116_amd64.deb";
+        version = "1.67.119";
+        url = "https://github.com/brave/brave-browser/releases/download/v1.67.119/brave-browser_1.67.119_amd64.deb";
         hash = "sha256-J4ap0bEAg5Q40U155AdAyzereBAo8Q/YDedDVqYvaT4=";
         platform = "x86_64-linux";
       }
